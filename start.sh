bfa5f2584ef1b689ee722e42989abf753704e40b
Lisa <106958298+LISA-KOREA@users.noreply.github.com>
2023-01-12 16:17:46 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+echo "Cloning Repo...."
+git clone https://github.com/LISA-KOREA/UPLOADER-BOT-V3 /UPLOADER-BOT-V3
+cd /UPLOADER-BOT-V3
+pip3 install -U -r requirements.txt
+echo "Starting Bot.... Please Wait. Check You Log. Bot Edit by @LISA_FAN_KL"
+python3 bot.py
