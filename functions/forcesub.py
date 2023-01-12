b2d1a0cb6bd0731381531e2b33d4b329cc02ddc9
Lisa <106958298+LISA-KOREA@users.noreply.github.com>
2023-01-12 16:39:49 +0530

    Create forcesub.py

---
+++ functions/forcesub.py
@@ -0,0 +1,49 @@
+import asyncio
+from plugins.config import Config
+from pyrogram import Client
+from pyrogram.errors import FloodWait, UserNotParticipant
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+async def handle_force_subscribe(bot, message):
+    try:
+        invite_link = await bot.create_chat_invite_link(int(Config.UPDATES_CHANNEL))
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        return 400
+    try:
+        user = await bot.get_chat_member(int(Config.UPDATES_CHANNEL), message.from_user.id)
+        if user.status == "kicked":
+            await bot.send_message(
+                chat_id=message.from_user.id,
+                text="Sorry Sir, You are Banned. Contact My [Support Group](https://t.me/NT_BOTS_SUPPORT).",
+                parse_mode="markdown",
+                disable_web_page_preview=True,
+                reply_to_message_id=message.message_id,
+            )
+            return 400
+    except UserNotParticipant:
+        await bot.send_message(
+            chat_id=message.from_user.id,
+            text="**Please Join My Update Channel To Use Me**",
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("✴️ Join My Update Channel ✴️", url=invite_link.invite_link)
+                    ]
+                ]
+            ),
+            parse_mode="markdown",
+            reply_to_message_id=message.message_id,
+        )
+        return 400
+    except Exception:
+        await bot.send_message(
+            chat_id=message.from_user.id,
+            text="Something Went Wrong. Contact My [Support Group](https://t.me/NT_BOTS_SUPPORT).",
+            parse_mode="markdown",
+            disable_web_page_preview=True,
+            reply_to_message_id=message.message_id,
+        )
+        return 400
+
+
