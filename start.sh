c04a9dc160f4877a3310593a3faed45128c2de48
mhdkurdish <49838432+mhdkurdish@users.noreply.github.com>
2023-01-27 02:34:08 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/LISA-KOREA/UPLOADER-BOT-V3 /UPLOADER-BOT-V3
+git clone https://github.com/mhdkurdish/UPLOADER-BOT-V3 /UPLOADER-BOT-V3
 cd /UPLOADER-BOT-V3
 pip3 install -U -r requirements.txt
-echo "Starting Bot.... Please Wait. Check You Log. Bot Edit by @LISA_FAN_KL"
+echo "Starting Bot.... Please Wait. Check You Log. Bot Edit by @mrbarznji"
 python3 bot.py
